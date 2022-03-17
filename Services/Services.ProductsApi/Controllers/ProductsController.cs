83c9b78caa466735c88b33cf2d844fa3c32ca6c0
cckamilo <cckamilo9@gmail.com>
2022-03-16 21:56:58 -0500

    insert para git

--- Services/Services.ProductsApi/Controllers/ProductsController.cs
+++ Services/Services.ProductsApi/Controllers/ProductsController.cs
@@ -73,7 +73,7 @@ namespace Services.ProductsApi.Controllers
                 }
 
             }
-            catch (Exception e)
+            catch
             {
                 return BadRequest();
             }
