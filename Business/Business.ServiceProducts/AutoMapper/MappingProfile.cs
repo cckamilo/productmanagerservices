dcb59a7f9dce442e8e2916da0f7a6d44f961257a
cckamilo <cckamilo9@gmail.com>
2022-03-27 11:26:57 -0500

    add clase Automapper

---
+++ Business/Business.ServiceProducts/AutoMapper/MappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using AutoMapper;
+using DataAccess.MongoDB.Models;
+using Models.ProductsApi.Models;
+
+namespace Business.ServiceProducts.AutoMapper
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<ItemsModel, Items>();
+            CreateMap<FileModel, File>();
+            CreateMap<ProductsModel, Products>();
+
+    }
+    }
+}
