b33f922a55b5abd0184f2f45f20e1784c0fc0539
cckamilo <cckamilo9@gmail.com>
2022-04-29 21:30:30 -0500

    lookup mongodb

---
+++ Repository/DataAccess.MongoDB/Lookup/ProductsLookup.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DataAccess.MongoDB.Interfaces;
+using DataAccess.MongoDB.Interfaces.Configuration;
+using DataAccess.MongoDB.Models;
+using MongoDB.Driver;
+
+namespace DataAccess.MongoDB.Lookup
+{
+    public class ProductsLookup : IProductsLookup
+    {
+        private readonly IMongoCollection<Products> productsCollection;
+        private readonly IMongoCollection<Categories> categoriesCollection;
+        public ProductsLookup(IStoreDataBaseSettings settings)
+        {
+            var mdbClient = new MongoClient(settings.connectionString);
+            var database = mdbClient.GetDatabase(settings.dataBaseName);
+            productsCollection = database.GetCollection<Products>("Products");
+            categoriesCollection = database.GetCollection<Categories>("Categories");
+
+        }
+
+        public async Task<IList<ProductsLookedUp>> GetProductsAsync()
+        {
+            try
+            {
+                var result = await productsCollection.Aggregate()
+                    .Lookup<Products, Categories, ProductsLookedUp>(categoriesCollection, a => a.categoryId, b => b.id, b => b.categories ).ToListAsync();
+                return result;
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return null;
+            }
+
+        }
+    }
+}
