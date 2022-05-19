c1ecdf2756650f68faddc0ab2438b4b2cfc71930
cckamilo <cckamilo9@gmail.com>
2022-05-18 20:49:06 -0500

    ajuste metodo categorias por id

--- Services/Services.ProductsApi/Controllers/CategoriesController.cs
+++ Services/Services.ProductsApi/Controllers/CategoriesController.cs
@@ -97,7 +97,7 @@ namespace Services.ProductsApi.Controllers
             {
                 var result = iCategoriesLogic.GetByIdAsync(id);
                 await Task.WhenAll(result);
-                return Ok(result.Result);
+                return Ok(result.Result.result);
             }
             else
             {
