d8168a5b989e84578c508d2ae3b96fffc30799a9
cckamilo <cckamilo9@gmail.com>
2022-06-21 22:09:23 -0500

    commit .net 6

---
+++ Business/Business.ServiceProducts/Exceptions/ApiException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+
+namespace Business.ServiceProducts.Exceptions
+{
+    public class ApiException : Exception
+    {
+        public ApiException() : base() { }
+
+        public ApiException(string message) : base(message) { }
+
+        public ApiException(string message, params object[] _args) : base(string.Format(CultureInfo.CurrentCulture, message, _args)) { }
+
+
+    }
+}
