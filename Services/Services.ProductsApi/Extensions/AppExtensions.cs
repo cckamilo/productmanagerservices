c5fed00fa23ac24c478bfb27301a3a4e288390f0
cckamilo <cckamilo9@gmail.com>
2022-06-21 20:18:03 -0500

    sube de version .netcore 6

---
+++ Services/Services.ProductsApi/Extensions/AppExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Services.ProductsApi.Middlewares;
+
+namespace Services.ProductsApi.Extensions
+{
+    public static class AppExtensions
+    {
+        public static void UseErrorHandlerMiddleware(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<ErrorHandlerMiddleware>();
+        }
+    }
+}
---
+++ Services/Services.ProductsApi/Middlewares/ErrorHandlerMiddleware.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Models.ProductsApi.ResponseModels;
+
+namespace Services.ProductsApi.Middlewares
+{
+    public class ErrorHandlerMiddleware
+    {
+
+        private readonly RequestDelegate _next;
+
+        public ErrorHandlerMiddleware(RequestDelegate next = null)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception error)
+            {
+                var response = context.Response;
+                response.ContentType = "application/json";
+                var responseError = new Error() { succeeded = false, message= error?.Message };
+                
+                switch (error)
+                {
+
+                    case Business.ServiceProducts.Exceptions.ApiException e:
+                        response.StatusCode = (int)HttpStatusCode.BadRequest;
+                        break;      
+                    case KeyNotFoundException e:
+                        response.StatusCode = (int)HttpStatusCode.NotFound;
+                        break;
+                    default:
+                        response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                        break;
+                }
+
+                //var result = JsonSerializer.Serialize(responseError);
+
+                await response.WriteAsJsonAsync(responseError);
+            }
+        }
+    }
+}
